fdd7078e98107946aad8f2a3e45cd6c027724395
Yukairon09 <yukarionn00@gmail.com>
2021-09-19 01:38:35 +0900

    change function from App to component/maptip_pllet

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Maptip_pallet from "./components/maptip_pallet";
 
 const App: React.FC = () => {
   return(
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Maptip_pallet />
     </div>
   )
 }
-
 
 export default App;

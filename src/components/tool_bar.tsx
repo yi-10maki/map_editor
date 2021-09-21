15fa71f3b55d8e9698d0c3fb53805910dc81c0ad
scrapzero <doragontuinatomikku@gmail.com>
2021-09-22 01:57:01 +0900

    kari

--- src/components/tool_bar.tsx
+++ src/components/tool_bar.tsx
@@ -3,11 +3,12 @@ import { Col, Button } from "react-bootstrap";
 import "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 /**関数名及びオブジェクト名は先頭大文字で
  * マップパチップパレット:マップチップを表示・選択する**/
 const Tool_bar: React.FC = () => {
   return (
-    <Col className="p-1"  style={{color: "$gray-900"}} xs={1} md={1}>
+    <Col style={{backgroundColor: "$gray-900"}} className="p-1" xs={1} md={1}>
         <Button className="m-1">描</Button>
         <Button className="m-1">消</Button>
         <Button className="m-1">選</Button>

f01f47833ff24da5658b7c490c2cb68bba9a07f5
Yukairon09 <yukarionn00@gmail.com>
2021-09-21 22:23:38 +0900

    delete png and modified App.css

--- src/components/maptip_pallet.tsx
+++ src/components/maptip_pallet.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ScrollContainer from "react-indiana-drag-scroll";
+
 /** min から max までの数字の入った配列を生成する関数 */
 const range = (min:number, max:number, step = 1) => {
   return Array.from(
@@ -10,16 +11,18 @@ const range = (min:number, max:number, step = 1) => {
   )
 }
 
+/**関数名及びオブジェクト名は先頭大文字で
+ * マップパチップパレット:マップチップを表示・選択する**/
 const Maptip_pallet: React.FC = () => {
   return (
     <div className="App">
-      <h1>React Indiana Drag Scrollデモ</h1>
       <div
         style={{ position: "relative", overflow: "hidden", border: "solid" }}
       >
-        {/** ScrollContainer でドラッグ範囲を括ります */}
+        {/** ScrollContainer でドラッグできる範囲を括ります */}
         <ScrollContainer ignoreElements={"#not-work-drag"}>
           <div style={{ display: "flex" }}>
+            {/** 0から100までのマップを表示*/}
             {range(0, 100).map((i) => (
               <Button style={{ margin: "1em .25em" }}>{i}</Button>
             ))}

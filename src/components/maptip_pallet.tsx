b5c18d62f41225d8b660e590c66776ec7b513217
scrapzero <doragontuinatomikku@gmail.com>
2021-09-25 18:58:45 +0900

    画像表示_読み込みはまだ

--- src/components/maptip_pallet.tsx
+++ src/components/maptip_pallet.tsx
@@ -3,17 +3,16 @@ import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ScrollContainer from "react-indiana-drag-scroll";
 
-/** min から max までの数字の入った配列を生成する関数 */
-const range = (min:number, max:number, step = 1) => {
-  return Array.from(
-    { length: (max - min + step) / step },
-    (v, k) => min + k * step
-  )
+function mapCSVToArray(csv: string): string[] {
+  return csv.split(',');
 }
 
 /**関数名及びオブジェクト名は先頭大文字で
  * マップパチップパレット:マップチップを表示・選択する**/
 const Maptip_pallet: React.FC = () => {
+  const img_names: string[] = mapCSVToArray("maptip1.png,maptip2.png,maptip3.png");
+  const image_edge_length : string="48";
+
   return (
     <div className="App">
       <div
@@ -23,8 +22,10 @@ const Maptip_pallet: React.FC = () => {
         <ScrollContainer ignoreElements={"#not-work-drag"}>
           <div style={{ display: "flex" }}>
             {/** 0から100までのマップを表示*/}
-            {range(0, 100).map((i) => (
-              <Button style={{ margin: "1em .25em" }}>{i}</Button>
+            {img_names.map((img_name) => (
+              <Button className="m-1">
+                <img src={`${process.env.PUBLIC_URL}/maptip/`+img_name} width={image_edge_length} height={image_edge_length} />
+              </Button>
             ))}
           </div>
         </ScrollContainer>

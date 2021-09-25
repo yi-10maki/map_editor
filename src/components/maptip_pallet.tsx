7434d024ba5e6211745ed2c5e9efa54f709b12a4
yi-10maki <ikeda_metakirby@yahoo.co.jp>
2021-09-25 23:15:31 +0900

    moudame

--- src/components/maptip_pallet.tsx
+++ src/components/maptip_pallet.tsx
@@ -11,9 +11,21 @@ const range = (min:number, max:number, step = 1) => {
   )
 }
 
+function fetchText(){
+  fetch(`${process.env.PUBLIC_URL}/TipList.txt`)
+  .then(response => {response.text()
+  })
+  .then(text => { alert("aaaaaaa")
+  });
+} 
+
 /**関数名及びオブジェクト名は先頭大文字で
  * マップパチップパレット:マップチップを表示・選択する**/
 const Maptip_pallet: React.FC = () => {
+  let data = fetchText(); 
+
+  const x: string[] = ["./csv", "josuehu"];
+  const y: string = "aaaa" + x;
   return (
     <div className="App">
       <div
@@ -23,12 +35,15 @@ const Maptip_pallet: React.FC = () => {
         <ScrollContainer ignoreElements={"#not-work-drag"}>
           <div style={{ display: "flex" }}>
             {/** 0から100までのマップを表示*/}
-            {range(0, 100).map((i) => (
-              <Button style={{ margin: "1em .25em" }}>{i}</Button>
+            {range(0, 100).map((i) => ( //0からcsvのリストのサイズまでにする
+              <Button style={{ margin: "1em .25em" }}>{i}</Button> //imgに変えたい
             ))}
           </div>
         </ScrollContainer>
+        
       </div>
+      <p className="csv"></p>
+      <p>data{y}{data}</p>
     </div>
   )
 }

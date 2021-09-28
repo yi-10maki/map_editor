91d3b899b0915241b47e0db47c1663cda279a6be
scrapzero <doragontuinatomikku@gmail.com>
2021-09-28 23:11:54 +0900

    maptip_component_ver1

--- src/components/maptip.tsx
+++ src/components/maptip.tsx
@@ -6,20 +6,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 /**関数名及びオブジェクト名は先頭大文字で
  * マップパチップパレット:マップチップを表示・選択する**/
-class Maptip extends React.Component {
+class Maptip extends React.Component<{id:number, img_name:string, image_edge_length:string},{id:number, img_name:string, image_edge_length:string}> {
   constructor(props: any){
     super(props);
     this.state = {
-      id: Number = this.props.id,
-      name: String,
-      image_edge_length :"48",
+      id: props.id,
+      img_name: props.img_name,
+      image_edge_length : props.image_edge_length,
     };
   }
-
+  
   render(){
     return (
       <Button className="m-1">
-        <img src={`${process.env.PUBLIC_URL}/maptip/`+name} width={image_edge_length} height={image_edge_length} />
+        <img 
+          src={`${process.env.PUBLIC_URL}/maptip/`+this.state.img_name} 
+          width={this.state.image_edge_length} 
+          height={this.state.image_edge_length} 
+        />
       </Button>
     )
   }
--- src/components/maptip_pallet.tsx
+++ src/components/maptip_pallet.tsx
@@ -1,35 +1,12 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+//import { Button } from "react-bootstrap";
+import Maptip from "./maptip";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ScrollContainer from "react-indiana-drag-scroll";
 
 
-function getCSV(file_path:string) {
-  return new Promise((resolve, reject) => {
-    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成、サーバと非同期通信するためのAPI
-    req.open("get", file_path, true); // アクセスするファイルを指定
-    req.onload = () => {
-      if (req.readyState === 4 && req.status === 0) {
-        resolve(mapCSVToArray(req.responseText));
-      } else {
-        reject(new Error(req.statusText));
-      }
-    };
-    req.onerror = () => {
-      reject(new Error(req.statusText));
-    };
-    req.send(null); // HTTPリクエストの発行
-  });
-}
-
-
 function mapCSVToArray(csv: string): string[] {
   return csv.split(',');
-}
-
-async function getData() {
-  const img_names: any=await getCSV(`${process.env.PUBLIC_URL}/maptip/TipList`);
-  return img_names;
 }
 
 /**関数名及びオブジェクト名は先頭大文字で
@@ -46,8 +23,8 @@ const Maptip_pallet: React.FC = () => {
         <ScrollContainer ignoreElements={"#not-work-drag"}>
           <div style={{ display: "flex" }}>
             {/** 0から100までのマップを表示*/}
-            {img_names.map((img_name) => (
-              <Maptip id=1 />
+            {img_names.map((img_name:string,id:number) => (
+              <Maptip id={id} img_name={img_name} image_edge_length={"48"}/>
             ))}
           </div>
         </ScrollContainer>
